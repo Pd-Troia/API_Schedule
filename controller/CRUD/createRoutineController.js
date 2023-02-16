5ff854e6646d61e0ace46dd7d8640c4d6499ca05
Hytalo Prates <pd_troia@hotmail.com>
2023-02-16 10:19:29 -0300

    createRoutineController now create the routine adding the last member free time  to 24h time in seconds

--- controller/CRUD/createRoutineController.js
+++ controller/CRUD/createRoutineController.js
@@ -2,8 +2,10 @@ const Routine = require('../../model/Routine')
 const generateFreeTime = require('../Features/free_time/generateFreeTime')
 const createRoutineController = async(req,res) =>{
     const {idUser,intervals} = req.body       
-    const intervalsWithSpace = generateFreeTime(intervals)
-    const routineModel = Routine({idUser,intervalsWithSpace})
+    const intervalsWithSpace = 
+    console.log(intervals)
+    console.log(intervalsWithSpace)
+    const routineModel = Routine({idUser,intervals:generateFreeTime(intervals)})
     try{
         await routineModel.save()
         res.status(200).json({msg:"Rotina criada com sucesso"})
