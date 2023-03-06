f489438c5d43713a878fe60b4566867b65eb9c39
Hytalo Prates <pd_troia@hotmail.com>
2023-03-06 11:45:59 -0300

    validation middleware create working

--- middlewares/validatorUserGroupMiddleware.js
+++ middlewares/validatorUserGroupMiddleware.js
@@ -22,37 +22,40 @@ const vRoutineObjectId = (resHandle,idRoutine) =>{
         return resHandle.status(400).json({msg:"IdRoutine não é um id válido"})
     }
 }
-const vUserExists = async(resHandle,idUser) =>{
+
+//validadte responses
+const validateCreateUserGroup = async(req,res,next) => {
+    const {idUser,idRoutine} = req.body
+    if(!idUser){
+        return res.status(400).json({msg:"IdUser não foi enviado"})
+    }
+    if(!idRoutine){
+        return res.status(400).json({msg:"IdRoutine não foi enviado"})
+    }
+    if(!mongoose.Types.ObjectId.isValid(idRoutine)){
+        return res.status(400).json({msg:"IdRoutine não é um id válido"})
+    }
+    if(!mongoose.Types.ObjectId.isValid(idRoutine)){
+        return res.status(400).json({msg:"IdRoutine não é um id válido"})
+    }   
     try{           
         const user = await userModel.getUser(idUser)
         if(!user){
-            return resHandle.status(404).json({msg:"O id do usuário é válido, porém não existe"}) 
+            return res.status(404).json({msg:"O id do usuário é válido, porém não existe"}) 
         }
     }catch(err){
         console.log(err)
     
     }
-}
-const vRoutineExists = async(resHandle,idRoutine) =>{
     try{           
         const routineExists = (await routineModel.getRoutineByIdRoutine(idRoutine)).length > 0        
         if(!routineExists){
-            return resHandle.status(404).json({msg:"O id da rotina é válido, porém não existe"}) 
+            return res.status(404).json({msg:"O id da rotina é válido, porém não existe"}) 
         }  
     }catch(err){
         console.log(err)
-        return resHandle.status(500).json({msg:"Ocorreu um erro no servidor"})         
+        return res.status(500).json({msg:"Ocorreu um erro no servidor"})         
     }
-}
-//validadte responses
-const validateCreateUserGroup = async(req,res,next) => {
-    const {idUser,idRoutine} = req.body
-    vUserPayload(res,idUser)
-    vRoutinePayload(res,idRoutine)
-    vUserObjectId(res,idUser)
-    vRoutineObjectId(res,idRoutine)    
-    vUserExists(res,idUser)
-    vRoutineExists(res,idRoutine)
     next()
 } 
 
