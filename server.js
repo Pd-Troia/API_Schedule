0ca087a5e0e460144e6668973a566758fb037e3e
Hytalo Prates <pd_troia@hotmail.com>
2023-03-11 18:02:10 -0300

    enviorments segregation

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose')
 const app = require('./app')
 
 //enviorment variables
-const user = process.env.DB_USER
-const password = process.env.DB_PASS 
+const user = process.env.ISDEV ?process.env.DB_USER_DEV : process.env.DB_USER
+const password = process.env.ISDEV ? process.env.DB_PASS_DEV : process.env.DB_PASS 
+const cluster = process.env.ISDEV ? process.env.CLUSTER_DEV : process.env.CLUSTER
 const port = process.env.PORT  
 //starting app
 mongoose.set("strictQuery", false);        
 mongoose
-.connect(`mongodb+srv://${user}:${password}@cluster0.qzrwirw.mongodb.net/?retryWrites=true&w=majority`)    
+.connect(`mongodb+srv://${user}:${password}@cluster0.${cluster}.mongodb.net/?retryWrites=true&w=majority`)    
 .then(()=>{
     app.listen(port || 3333)
     console.log("App listen on port " + port)
