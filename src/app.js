807cde01073935c15364b395a8e9d257e83cb234
Hytalo Prates <pd_troia@hotmail.com>
2023-03-13 11:01:25 -0300

    swagger added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require("./swagger.json")
 const router = require('./router') 
 
 const app = express()
 //configs 
 app.use(express.json())
 app.use(cors())
+app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerFile))
 app.use(router)
 
 
