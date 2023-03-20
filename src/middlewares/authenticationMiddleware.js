74b5d9cee4ab7e6780e7333e6ab3545e734b1fbd
Hytalo Prates <pd_troia@hotmail.com>
2023-03-20 10:34:18 -0300

    exp token verify

--- src/middlewares/authenticationMiddleware.js
+++ src/middlewares/authenticationMiddleware.js
@@ -6,13 +6,17 @@ const authenticateUser = (req,res,next) =>{
         return res.status(401).json({msg:'Token não fornecido'})
     }
     try{
-        const decoded = jwt.decode(token,secret)        
+        jwt.verify(token,secret,{algorithms:"HS256"})           
+        const decoded = jwt.decode(token)        
         if(decoded){
             req.user = decoded
-            next()
         }else{
             return res.status(401).json({msg:'Token inválido'})
+        }                
+        if(Math.floor(Date.now()/1000) > decoded.exp){
+            return res.status(401).json({msg:'Token expirado'})
         }
+        next()        
     }catch(err){
         console.log(err)
         return res.status(401).json({msg:'Token inválido'})
