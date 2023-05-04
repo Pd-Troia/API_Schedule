89cb8c535c158ed1456d306f986910dfd53dddfb
Hytalo Prates <pd_troia@hotmail.com>
2023-05-04 11:44:46 -0300

    working

--- src/Routes/notification.router.js
+++ src/Routes/notification.router.js
@@ -1,14 +1,15 @@
 const express = require('express')
+const notificationController = require('../controllers/notificationController') 
 
 const router = express.Router()
 
 // create notification
-router.post("/notification")
+router.post("/notification",notificationController.createNotification)
 
 //get notification "web socket"
 router.get("/notification")
 
 //Delete notification 
-router.delete("/notification")
+router.delete("/notification",notificationController.deleteNotification)
 
 module.exports = router
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require("./swagger.json")
 const routineRoutes = require('./Routes/routine.router') 
 const userGroupRoutes = require('./Routes/userGroups.router')
+const notificationRoutes = require('./Routes/notification.router')
 
 const app = express()
 //configs 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerFile))
 app.use(routineRoutes)
 app.use(userGroupRoutes)
+app.use(notificationRoutes)
 
 
 
